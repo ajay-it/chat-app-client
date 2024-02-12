59983065614cd7af2258b05ea723e2fe93c7a2c7
Ajay Kushwaha <ajkushwaha1112@gmail.com>
2024-02-13 00:25:42 +0530

    sending and receiving messages

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,13 +1,17 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Avatar from "./Avatar";
 import Logo from "./Logo";
 import { UserContext } from "./UserContext";
+import { uniqBy } from "lodash";
 
 export default function Chat() {
   const [ws, setWs] = useState(null);
   const [onlinePeople, setOnlinePeople] = useState([]);
   const [selectedUserId, setSelectedUserId] = useState();
   const { id } = useContext(UserContext);
+  const [newMessageText, setNewMessageText] = useState("");
+  const [messages, setMessages] = useState([]);
+  const divUnderMessages = useRef();
 
   function showOnlinePeople(peopleArray) {
     const people = {};
@@ -19,12 +23,44 @@ export default function Chat() {
 
   function handleMessage(ev) {
     const messageData = JSON.parse(ev.data);
+    console.log("messageData", { ev, messageData });
     if ("online" in messageData) {
       showOnlinePeople(messageData.online);
+    } else if ("text" in messageData) {
+      setMessages((prev) => [...prev, { ...messageData }]);
     }
   }
   const onlinePeopleExclUser = { ...onlinePeople };
   delete onlinePeopleExclUser[id];
+
+  const messagesWithoutDupes = uniqBy(messages, "id");
+
+  function sendMessage(ev) {
+    ev.preventDefault();
+    ws.send(
+      JSON.stringify({
+        recipient: selectedUserId,
+        text: newMessageText,
+      })
+    );
+    setNewMessageText("");
+    setMessages((prev) => [
+      ...prev,
+      {
+        text: newMessageText,
+        sender: id,
+        recipient: selectedUserId,
+        id: Date.now(),
+      },
+    ]);
+  }
+
+  useEffect(() => {
+    const div = divUnderMessages.current;
+    if (div) {
+      div.scrollIntoView({ behavior: "smooth", block: "end" });
+    }
+  }, [messages]);
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:4000");
@@ -62,30 +98,62 @@ export default function Chat() {
               </div>
             </div>
           )}
+          {!!selectedUserId && (
+            <div className="relative h-full">
+              <div className="overflow-y-scroll absolute inset-0">
+                {messagesWithoutDupes.map((message, index) => (
+                  <div
+                    key={index}
+                    className={`${
+                      message.sender === id ? "text-right" : "text-left"
+                    }`}
+                  >
+                    <div
+                      className={`p-2 my-2 text-left inline-block rounded-md text-sm ${
+                        message.sender === id
+                          ? "bg-blue-500 text-white"
+                          : "bg-white text-gray-500"
+                      }`}
+                    >
+                      {message.text}
+                    </div>
+                  </div>
+                ))}
+                <div ref={divUnderMessages}></div>
+              </div>
+            </div>
+          )}
         </div>
-        <div className="flex gap-2">
-          <input
-            type="text"
-            placeholder="Type your message here"
-            className="bg-white border p-2 rounded flex-grow"
-          ></input>
-          <button className="bg-blue-500 p-2 rounded text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
+        {!!selectedUserId && (
+          <form className="flex gap-2" onSubmit={sendMessage}>
+            <input
+              type="text"
+              value={newMessageText}
+              onChange={(ev) => setNewMessageText(ev.target.value)}
+              placeholder="Type your message here"
+              className="bg-white border p-2 rounded flex-grow"
+            ></input>
+            <button
+              type="submit"
+              className="bg-blue-500 p-2 rounded text-white"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-              />
-            </svg>
-          </button>
-        </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                />
+              </svg>
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
